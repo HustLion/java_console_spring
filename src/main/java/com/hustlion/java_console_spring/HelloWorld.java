185c31326085b23948d27aa0b6c42728bb54206d
HustLion <hustlionm@qq.com>
2017-05-28 18:41:21 +0800

    first commit

---
+++ src/main/java/com/hustlion/java_console_spring/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.hustlion.java_console_spring;
+
+public class HelloWorld {
+	private String message;
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void getMessage() {
+		System.out.println("Your Message : " + message);
+	}
+
+}
